297910341c15a78c864aefd0f0108e852bccc551
Ricardo Kaeriyama Hirano <ricardohiranorgt@gmail.com>
2026-03-16 17:01:03 -0300

    @ricardohirano Estuturando basicamente

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
+
 const app = express();
+const port = 3000;
+
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", function(req, res){
-    res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-const porta = 8080;
-app.listen(porta,function(erro){
-    if(erro){
-        console.log("Ocorreu um erro!");
-    } else {
-        console.log(`Servidor iciado com sucesso pagina http://localhost:${porta} `)
-    }
-})
+app.get('/sobre', (req, res) => {
+  res.render('sobre');
+});
+
+app.get('/equipe', (req, res) => {
+  res.render('equipe');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
