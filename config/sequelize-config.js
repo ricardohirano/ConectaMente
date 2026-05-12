a8b62362c67cf54ef45b543b30efc3f526b7a9a2
Ricardo Hirano <ricardohiranorgt@gmail.com>
2026-05-11 21:54:22 -0300

    @ricardohirano comeco do relatorio da crianca

--- config/sequelize-config.js
+++ config/sequelize-config.js
@@ -3,7 +3,7 @@ import Sequelize from "sequelize";
 const connection = new Sequelize({
   dialect: "mysql",
   host: "localhost",
-  port: 3307, /*mudar para 3306 na fatec  e outros lugares*/
+  port: 3306, /*mudar para 3306 na fatec  e outros lugares*/
   username: "root",
   password: "",
   database: "conectamente",
